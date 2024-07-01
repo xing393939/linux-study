1df13dc9f78100de23b57d666f3192e6e40f5280
xing393939 <xing393939@users.noreply.github.com>
2024-07-01 11:17:47 +0800

    kernel debug

--- bpf/hello.bpf.c
+++ bpf/hello.bpf.c
@@ -6,8 +6,7 @@
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 SEC("tp/syscalls/sys_enter_execve")
-int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter
-					   *ctx)
+int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter *ctx)
 {
 	const char *filename = (const char *)(ctx->args[0]);
 	pid_t pid = bpf_get_current_pid_tgid();
