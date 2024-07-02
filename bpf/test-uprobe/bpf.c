517caff99b507f09285b2a81f74ebb738a7e4f76
chenxing <chenxing@bookan.com.cn>
2024-07-02 17:11:14 +0800

    get args

--- bpf/test-uprobe/bpf.c
+++ bpf/test-uprobe/bpf.c
@@ -1,10 +1,14 @@
+#define __TARGET_ARCH_x86 1
 #include <linux/bpf.h>
+#include <linux/ptrace.h>
 #include "bpf/bpf_helpers.h"
+#include "bpf/bpf_tracing.h"
+
 char __license[] SEC("license") = "Dual MIT/GPL";
 
 SEC("uprobe/go_test_greet")
-int bpf_prog(void *ctx) {
-    char buf[] = "Hello World!\n";
-    bpf_trace_printk(buf, sizeof(buf));
+int bpf_prog(struct pt_regs *ctx) {
+    char buf[] = "Hello %d %d \n";
+    bpf_trace_printk(buf, sizeof(buf), PT_REGS_PARM1(ctx), PT_REGS_PARM2(ctx));
     return 0;
 }
