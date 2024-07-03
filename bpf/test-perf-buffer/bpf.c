8b2b18ee7b2dead111e6b6fc9e2a3ea5f6ca37a8
xing393939 <xing393939@users.noreply.github.com>
2024-07-03 11:59:22 +0800

    ebpf perf buffer

---
+++ bpf/test-perf-buffer/bpf.c
@@ -0,0 +1,46 @@
+#define __TARGET_ARCH_x86 1
+#include <linux/bpf.h>
+#include <linux/types.h>
+#include <linux/ptrace.h>
+#include "bpf/bpf_helpers.h"
+#include "bpf/bpf_tracing.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct event {
+    int pid;
+    int ppid;
+    int uid;
+    int retval;
+    __u8 comm[16];
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(key_size, sizeof(__u32));
+    __uint(value_size, sizeof(__u32));
+} events SEC(".maps");
+
+// Force emitting struct event into the ELF.
+const struct event *unused __attribute__((unused));
+
+SEC("uprobe/go_test_greet")
+int bpf_prog(struct pt_regs *ctx) {
+    __u64 id;
+    int pid, tgid;
+    struct event event={0};
+    struct task_struct *task;
+
+    int uid = (__u32)bpf_get_current_uid_gid();
+    id = bpf_get_current_pid_tgid();
+    tgid = id >> 32;
+
+    event.pid = tgid;
+    event.uid = uid;
+    task = (struct task_struct*)bpf_get_current_task();
+
+    bpf_get_current_comm(&event.comm, sizeof(event.comm));
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
+
+    return 0;
+}
---
+++ bpf/test-perf-buffer/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/perf"
+	"github.com/cilium/ebpf/rlimit"
+	"log"
+)
+import "C"
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -type event ebpf bpf.c
+
+func main() {
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	path := "/root/logs/test2/a.out"
+	ex, err := link.OpenExecutable(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	objs := ebpfObjects{}
+	if err := loadEbpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %v", err)
+	}
+	defer objs.Close()
+
+	_, err = ex.Uprobe("add", objs.BpfProg, &link.UprobeOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rd, err := perf.NewReader(objs.Events, 1024)
+	if err != nil {
+		log.Fatalf("opening ringbuf reader: %s", err)
+	}
+	defer rd.Close()
+	var event ebpfEvent
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			log.Printf("reading from reader: %s", err)
+			return
+		}
+
+		// Parse the ringbuf event entry into a bpfEvent structure.
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing ringbuf event: %s", err)
+			continue
+		}
+
+		log.Printf("pid: %d\tcomm: %s\n", event.Pid, string(event.Comm[:]))
+	}
+}
