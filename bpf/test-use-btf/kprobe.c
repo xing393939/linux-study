3284fd144e5f24297fab635ba969840ff7dc6fdc
chenxing <chenxing@bookan.com.cn>
2024-07-04 10:09:11 +0800

    1

--- bpf/test-use-btf/kprobe.c
+++ bpf/test-use-btf/kprobe.c
@@ -7,14 +7,14 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 
 SEC("kprobe/sys_execve")
 int kprobe_execve() {
-	__u64 id = bpf_get_current_pid_tgid();
-	__u64 ppid;
+	pid_t pid = bpf_get_current_pid_tgid();
+	__u32 cpu;
     struct task_struct *task;
     task = (struct task_struct *) bpf_get_current_task();
-    ppid = BPF_CORE_READ(task, tgid);
+    cpu = BPF_CORE_READ(task, cpu);
 
-    char buf[] = "Hello %d %d \n";
-    bpf_trace_printk(buf, sizeof(buf), id, ppid);
+    char buf[] = "pid=%d cpu=%d \n";
+    bpf_trace_printk(buf, sizeof(buf), pid, cpu);
 
 	return 0;
 }
